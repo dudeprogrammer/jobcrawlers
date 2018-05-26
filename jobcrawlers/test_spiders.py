4d3333fec82dd6503b48fc10da3dbf7cdeff76ca
Mohamad <mohamad75124@gmail.com>
2018-05-27 03:22:07 +0430

    Added unit test for Stackoverflow specific job parser

--- jobcrawlers/test_spiders.py
+++ jobcrawlers/test_spiders.py
@@ -28,3 +28,31 @@ class StackoverflowSpiderTest(unittest.TestCase):
         with open('sample_pages/job_list_res.txt') as file:
             result = file.read()
         self.assertEqual(items, result)
+
+    def test_stackoverflow_job_page(self):
+        """Test Stackoverflow.parse_job_page
+
+            Note that it depends on sample_pages/stackoverflow_job_list.html sample file and
+            sample_pages/job_page_res.json.
+        """
+
+        with open('sample_pages/stackoverflow_job_page.html', 'r') as file:
+            http_body = file.read()
+        url = 'https://stackoverflow.com/jobs/172695/mobile-qa-engineer-to-guard-the-quality-of-our-app-annie'
+        request = Request(url=url)
+        response = TextResponse(url, body=http_body, request=request, encoding='utf-8')
+        items = self.spider.parse_job_page(response)
+        item = list(items)[0]
+
+        with open('sample_pages/job_page_res.json', 'r') as file:
+            data = json.load(file)
+        self.assertEqual(data['company_size'], item['company_size'])
+        self.assertEqual(data['company_type'], item['company_type'])
+        self.assertEqual(data['date_scraped'], item['date_scraped'])
+        self.assertEqual(data['experience'], item['experience'])
+        self.assertEqual(data['industry'], item['industry'])
+        self.assertEqual(data['job_type'], item['job_type'])
+        self.assertEqual(data['link'], item['link'])
+        self.assertEqual(data['role'], item['role'])
+        self.assertEqual(data['tags'], item['tags'])
+        self.assertEqual(data['title'], item['title'])
